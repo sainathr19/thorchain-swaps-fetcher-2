cc1b9b79bbd6d071c9b19b51018e78251ab8d8ec
Sainath Reddy <gsnr1925@gmail.com>
2024-11-26 12:04:48 +0530

    migrated to postgresql

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,14 +1,15 @@
-use crate::db::MySQL;
+use crate::db::PostgreSQL;
 use crate::models::actions_model::SwapTransactionFromatted;
 use crate::utils::midgard::MidGard;
-use crate::utils::transaction_handler::{TransactionError, TransactionHandler};
+use crate::utils::transaction_handler::{TransactionError, TransactionHandler, PENDING_TRANSACTION_IDS};
 use crate::utils::{read_next_page_token_from_file, write_next_page_token_to_file};
 use chrono::Utc;
 
 pub async fn fetch_historical_data() -> Result<(), TransactionError> {
-    let mysql = MySQL::init().await.map_err(|e| {
-        TransactionError::DatabaseError(format!("Error connecting to MySQL: {:?}", e))
+    let pg = PostgreSQL::init().await.map_err(|e| {
+        TransactionError::DatabaseError(format!("Error connecting to PostgreSQL: {:?}", e))
     })?;
+    let transaction_handler = TransactionHandler;
     const TOKEN_FILE_PATH: &str = "next_page_token.txt";
     let mut next_page_token = read_next_page_token_from_file(TOKEN_FILE_PATH).unwrap_or_default();
 
@@ -28,7 +29,7 @@ pub async fn fetch_historical_data() -> Result<(), TransactionError> {
         }
 
         let process_response =
-            TransactionHandler::process_and_insert_transaction(&mysql, &resp.actions).await;
+            transaction_handler.process_and_insert_transaction(&pg, &resp.actions).await;
 
         match process_response {
             Ok(_) => {
@@ -52,11 +53,11 @@ pub async fn fetch_historical_data() -> Result<(), TransactionError> {
 
     Ok(())
 }
-
-pub async fn fetch_latest_data(mysql: &MySQL) -> Result<(), TransactionError> {
-    let latest_timestamp = match mysql.fetch_latest_timestamp().await {
+pub async fn fetch_latest_data(pg: &PostgreSQL) -> Result<(), TransactionError> {
+    let transaction_handler = TransactionHandler;
+    let latest_timestamp = match pg.fetch_latest_timestamp().await {
         Ok(Some(timestamp)) => timestamp,
-        Ok(None) => Utc::now().timestamp() as i64,
+        Ok(None) => Utc::now().timestamp() as i32,
         Err(err) => {
             return Err(TransactionError::DatabaseError(format!(
                 "Error fetching the latest timestamp: {:?}",
@@ -65,7 +66,7 @@ pub async fn fetch_latest_data(mysql: &MySQL) -> Result<(), TransactionError> {
         }
     };
 
-    let mysql_clone = mysql.clone();
+    let pg_clone = pg.clone();
     let latest_timestamp_str = latest_timestamp.to_string();
 
     // Fetch actions with the latest timestamp
@@ -81,7 +82,7 @@ pub async fn fetch_latest_data(mysql: &MySQL) -> Result<(), TransactionError> {
     let mut actions = resp.actions.clone();
     actions.reverse();
     let process_response =
-        TransactionHandler::process_and_insert_transaction(&mysql_clone, &actions).await;
+        transaction_handler.process_and_insert_transaction(&pg_clone, &actions).await;
     match process_response {
         Ok(_) => (),
         Err(err) => {
@@ -105,7 +106,7 @@ pub async fn fetch_latest_data(mysql: &MySQL) -> Result<(), TransactionError> {
         };
 
         let process_response =
-            TransactionHandler::process_and_insert_transaction(&mysql_clone, &resp.actions).await;
+            transaction_handler.process_and_insert_transaction(&pg_clone, &resp.actions).await;
         match process_response {
             Ok(_) => (),
             Err(err) => {
@@ -120,10 +121,40 @@ pub async fn fetch_latest_data(mysql: &MySQL) -> Result<(), TransactionError> {
     println!("Latest Data Updated at : {}", latest_timestamp_str);
     Ok(())
 }
+pub async fn retry_pending_transactions(pg: &PostgreSQL) -> Result<(), TransactionError> {
+    let transaction_handler = TransactionHandler;
+    let pending_txn_ids = PENDING_TRANSACTION_IDS.lock().await.clone();
+    println!("Fetching Pending Transactions.. : {:?}",&pending_txn_ids);
+    
+    for transaction_id in pending_txn_ids {
+        let resp = match MidGard::fetch_action_with_transactionid(transaction_id).await {
+            Ok(response) => response,
+            Err(err) => {
+                return Err(TransactionError::ApiError(format!(
+                    "Error fetching previous page actions: {:?}",
+                    err
+                )));
+            }
+        };
+        let pg_clone = pg.clone();
+        let process_response =
+            transaction_handler.process_and_insert_transaction(&pg_clone, &resp.actions).await;
+        match process_response {
+            Ok(_) => (),
+            Err(err) => {
+                return Err(TransactionError::ProcessingError(format!(
+                    "Error processing transaction: {:?}",
+                    err
+                )));
+            }
+        };
+    }
+    Ok(())
+}
+pub async fn fetch_from_start(pg: &PostgreSQL) -> Result<(), TransactionError> {
+    let transaction_handler = TransactionHandler;
 
-
-pub async fn fetch_from_start(mysql: &MySQL) -> Result<(), TransactionError> {
-    let mysql_clone = mysql.clone();
+    let pg_clone = pg.clone();
     let start_timestamp = "1700357476";
     const TOKEN_FILE_PATH: &str = "prev_page_token.txt";
     
@@ -133,7 +164,7 @@ pub async fn fetch_from_start(mysql: &MySQL) -> Result<(), TransactionError> {
         match MidGard::fetch_actions_with_timestamp(&start_timestamp).await {
             Ok(response) => {
                 let actions = response.actions.clone();
-                match TransactionHandler::process_and_insert_transaction(&mysql_clone, &actions).await {
+                match transaction_handler.process_and_insert_transaction(&pg_clone, &actions).await {
                     Ok(_) => {
                         if let Err(e) = write_next_page_token_to_file(&response.meta.prevPageToken, TOKEN_FILE_PATH) {
                             return Err(TransactionError::FileError(format!(
@@ -184,7 +215,7 @@ pub async fn fetch_from_start(mysql: &MySQL) -> Result<(), TransactionError> {
                 )));
             }
         };
-        let processed_transactions = TransactionHandler::process_transactions(&resp.actions).await;
+        let processed_transactions = transaction_handler.process_transactions(&resp.actions).await;
         match processed_transactions{
             Ok(val)=>{
                 transaction_batch.extend(val);
@@ -207,7 +238,7 @@ pub async fn fetch_from_start(mysql: &MySQL) -> Result<(), TransactionError> {
         }
 
         if batch_count>=20{
-            let insertion_response = mysql.insert_bulk(transaction_batch.clone()).await;
+            let insertion_response = pg.insert_bulk(transaction_batch.clone()).await;
             match insertion_response {
                 Ok(_)=>{
                     println!("Batch insertion Successfull of : {}",&transaction_batch.len());
